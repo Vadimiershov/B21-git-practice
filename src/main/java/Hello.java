0aeaa41fc5c9566d2a00a6a826898c82b8b1c233
Vadimiershov <40811461+Vadimiershov@users.noreply.github.com>
2021-01-26 14:23:52 -0600

    master branch, spouce changed the location

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world B22");
         System.out.println("B21 future SDET, learning GIT");
+        //
     }
 
 
